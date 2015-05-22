e63394694643d706bfd4f6d1501804d283537f0d
T-107 <T-107@PC254>
2015-05-21 19:43:17 -0500

    segunda clase

---
+++ src/abstraccion/interfazImc.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstraccion;
+
+/**
+ *
+ * @author T-107
+ */
+public class interfazImc {
+    
+    public static void main(String oso[]){
+  
+       Imc imc=new Imc(80,1.67f);
+       
+        System.out.println(  imc.calcular()  );
+        
+        
+    }
+    
+    
+    
+}
+
+
